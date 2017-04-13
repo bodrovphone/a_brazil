a2a2ec76d25f8e7847cf15b68d9fae33fed174ab
obodrov <bodrovphone@gmail.com>
2017-04-13 16:41:15 +0200

    4.3.7 added cse

--- js/main.js
+++ js/main.js
@@ -13,11 +13,17 @@ $(document).ready(function(){
     $('.parent').on('click', function(e){
         $(this).toggleClass('active');
         $(this).find('ul').slideToggle();
-        
     });
 
     $("#back-to-top").click(function (e) {
         e.preventDefault();
         $("html, body").animate({scrollTop: 0}, 1000);
+    });
+    
+    $('.custom-cse-toggle img').on('click', function(e){
+        e.preventDefault();
+        $('.custom-cse').toggleClass('cse-display');
+        if($('.cse-display').size() > 0) $(this).attr('src', 'img/cancel_search.svg');
+        else $(this).attr('src', 'img/search.svg'); 
     });
 });
